1d63e094b586cc742d060ca182abfa3ce8660499
abyjacobkallarackal <43222811+abyjacobkallarackal@users.noreply.github.com>
2026-02-07 01:15:32 +0530

    Add files via upload
    main pages

---
+++ propose.js
@@ -0,0 +1,22 @@
+const text =
+  "Joash, will you always be my today, tomorrow, and forever?";
+
+let i = 0;
+const speed = 60;
+
+function typeText() {
+  if (i < text.length) {
+    document.getElementById("proposalText").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeText, speed);
+  }
+}
+
+window.onload = () => {
+  typeText();
+};
+
+function sheSaidYes() {
+  document.getElementById("bgMusic").play();
+  alert("Forever starts now 💍🤍");
+}
